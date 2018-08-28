f5004fd0d90717207f1f4b4af3e9e80fe8e4ece9
aluno <aluno@Lab-eic>
2018-08-28 20:33:29 -0300

    Teste de Git

---
+++ Interação Humano-Computador(T1)/src/interação/humano/computador/t1/InteraçãoHumanoComputadorT1.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package interação.humano.computador.t1;
+
+/**
+ *
+ * @author aluno
+ */
+public class InteraçãoHumanoComputadorT1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
